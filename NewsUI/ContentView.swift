156be291dacb4df476b7e45960470cdd2d0bb3a9
mtfum <f.yamanaka1127@gmail.com>
2021-06-22 21:41:54 -0700

    Initial Commit

---
+++ NewsUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NewsUI
+//
+//  Created by Fumiya Yamanaka on 2021/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NewsUI/NewsUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsUIApp.swift
+//  NewsUI
+//
+//  Created by Fumiya Yamanaka on 2021/06/22.
+//
+
+import SwiftUI
+
+@main
+struct NewsUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
